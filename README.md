3c08e6c3ef6f86f9bab532b5685608325aa87261
Marucik <rakoczy.mk@gmail.com>
2017-01-26 19:26:47 +0100

    Działający .gitignore

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Ulatwiaczek
-
-## Bo ułatwia ( ͡~ ͜ʖ ͡°)
+Projekt w Pythonie
---
+++ virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\webdeveloping\git repos\ulatwiaczek\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
