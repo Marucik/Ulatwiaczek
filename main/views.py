1b8816bfc31352059539d3e9fc979735cda51c3d
Marucik <rakoczy.mk@gmail.com>
2017-01-29 19:08:01 +0100

    Dodano widoki

--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("<h1>Hello, world.</h1>")
+
+def test_dodaj(request):
+    return HttpResponse("<h1>Dodawanie testu</h1>")
+
+def test_lista(request):
+    return HttpResponse("<h1>Lista testów</h1>")
+
+def sprawdzian_dodaj(request):
+    return HttpResponse("<h1>Dodawanie sprawdzianu</h1>")
+
+def sprawdzian_lista(request):
+    return HttpResponse("<h1>Lista sprawdzianów</h1>")
