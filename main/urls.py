a55b8c471fc367266877418c4fd7181064df2749
Marucik <rakoczy.mk@gmail.com>
2017-01-29 19:24:27 +0100

    Mała wpadka, ale poprawka :)

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^ulatwiaczek/$', views.index, name='index'),
-    url(r'^ulatwiaczek/test/dodaj/$', views.test_dodaj, name='test_dodaj'),
+    url(r'^$', views.index, name='index'),
+    url(r'^test/dodaj/$', views.test_dodaj, name='test_dodaj'),
     url(r'^test/lista/$', views.test_lista, name='test_lista'),
     url(r'^sprawdzian/dodaj/$', views.sprawdzian_dodaj, name='sprawdzian_dodaj'),
     url(r'^sprawdzian/lista/$', views.sprawdzian_lista, name='sprawdzian_lista'),
