9bf4d24785dc12d1b4d0f541a868550019b54228
Marucik <rakoczy.mk@gmail.com>
2017-01-29 19:07:54 +0100

    Dodano adresy do widoków

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^ulatwiaczek/$', views.index, name='index'),
+    url(r'^ulatwiaczek/test/dodaj/$', views.test_dodaj, name='test_dodaj'),
+    url(r'^test/lista/$', views.test_lista, name='test_lista'),
+    url(r'^sprawdzian/dodaj/$', views.sprawdzian_dodaj, name='sprawdzian_dodaj'),
+    url(r'^sprawdzian/lista/$', views.sprawdzian_lista, name='sprawdzian_lista'),
 ]
