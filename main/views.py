00872ad56c78985371fa41dc2b16f05eddb0b91c
Dawiducik <edekdw@gmail.com>
2017-01-31 22:22:34 +0100

    Dodano dekoratory login_required

--- main/views.py
+++ main/views.py
@@ -1,34 +1,55 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return HttpResponse("<h1>Hello, world. Only 4 peasants!</h1>")
+
+def logowanie(request):
+	return HttpResponse("Logowanie HERE")
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def index_logged(request):
     return HttpResponse("<h1>Hello, world. Only 4 logged users. Super Secret.</h1>")
 
-
-
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def test_dodaj(request):
     return HttpResponse("<h1>Dodawanie testu</h1>")
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def test_lista(request):
     return HttpResponse("<h1>Lista testów</h1>")
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def test_szczegoly(request, id):
 	return HttpResponse("<h1> Szczegoly testu numerek %s </h1>"  % id)
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def test_usun(request, id):
 	return HttpResponse("<h1> Usuwanko testu numerek %s </h1>" % id)
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def test_edytuj(request, id):
 	return HttpResponse("<h1> Edytowanko testu numerek %s </h1>" % id)
 
-
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def sprawdzian_dodaj(request):
     return HttpResponse("<h1>Dodawanie sprawdzianu</h1>")
+
+@login_required(login_url='/ulatwiaczek/logowanie/')   
 def sprawdzian_lista(request):
     return HttpResponse("<h1>Lista sprawdzianów</h1>")
+
+@login_required(login_url='/ulatwiaczek/logowanie/') 
 def sprawdzian_szczegoly(request, id):
 	return HttpResponse("<h1> Szczegoly sprawdzianu numerek %s </h1>" % id)
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def sprawdzian_usun(request, id):
 	return HttpResponse("<h1> Usuwanko sprawdzianu numerek %s </h1>" % id)
+
+@login_required(login_url='/ulatwiaczek/logowanie/')
 def sprawdzian_edytuj(request, id):
 	return HttpResponse("<h1> Edytowanko sprawdzianku numerek %s </h1>" % id)
