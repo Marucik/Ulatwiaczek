8be67bcccf92918b80f45599ae07c79e8b57b64b
Marucik <rakoczy.mk@gmail.com>
2017-01-31 19:42:50 +0100

    Zmiana Urls admina z głównego na main

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib import admin
+
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index_logged, name='index_logged'),
-  
+
     url(r'^test/$', views.test_lista, name='test_lista'),
     url(r'^test/dodaj/$', views.test_dodaj, name='test_dodaj'),
     url(r'^test/(?P<id>[1-9]+)/$', views.test_szczegoly, name='test_szczegoly'),
@@ -16,4 +18,6 @@ urlpatterns = [
     url(r'^sprawdzian/(?P<id>[1-9]+)/$', views.sprawdzian_szczegoly, name='sprawdzian_szczegoly'),
     url(r'^sprawdzian/(?P<id>[1-9]+)/usun/$', views.sprawdzian_usun, name='sprawdzian_usun'),
     url(r'^sprawdzian/(?P<id>[1-9]+)/edytuj/$', views.sprawdzian_edytuj, name='sprawdzian_edytuj'),
+
+    url(r'^admin/', admin.site.urls),
 ]
