3fd98b1d3d96abcb7461aaa9aea5eddca50617e5
Marucik <rakoczy.mk@gmail.com>
2017-02-17 21:39:38 +0100

    Dodanie nowej komendy do gulpfile i modyfiakcja komendy 'sass:watch'

--- gulpfile.js
+++ gulpfile.js
@@ -49,12 +49,30 @@ gulp.task('font-awesome', function () {
          .pipe(rename({suffix: '.min'}))
          .pipe(gulp.dest('main/static/css'));
 });
+gulp.task('sass:login', function () {
+   return gulp.src('sources/style/login.scss')
+         .pipe(sass().on('error', sass.logError))
+         .pipe(postcss([ autoprefixer() ]))
+		   .pipe(cssbeautify({
+            indent: '  ',
+            openbrace: 'end-of-line',
+            autosemicolon: true
+          }))
+         .pipe(gulp.dest('main/static/css'))
+         .pipe(cssmin())
+         .pipe(rename({suffix: '.min'}))
+         .pipe(gulp.dest('main/static/css'));
+});
 gulp.task('sass:watch', function () {
-   gulp.watch('sources/style/*.scss',['sass']);
+   gulp.watch('sources/style/*.scss',['sass', 'sass:login']);
    gulp.watch('node_modules/bootstrap-sass/assets/stylesheets/**/*.scss',['bootstrap-sass']);
 });
 
 gulp.task('fonts', function() {
     return gulp.src('node_modules/font-awesome/fonts/*')
     .pipe(gulp.dest('main/static/fonts'))
+});
+
+gulp.task('compile_css', ['sass', 'sass:login', 'font-awesome'], function () {
+    console.log("Kompiluje pliki scss.");
 });
