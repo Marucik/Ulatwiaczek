ffbb60cdee95749c73c635f809b6c7309fc8156b
Dawiducik <edekdw@gmail.com>
2017-02-18 12:52:45 +0100

    Dodano wylogowywanie

--- main/urls.py
+++ main/urls.py
@@ -8,14 +8,15 @@ urlpatterns = [
     url(r'^$', views.index_logged, name='index_logged'),
 
     url(r'^logowanie/$', views.logowanie, name="logowanie"),
+    url(r'^wylogowanie/$', views.wylogowanie, name="wylogowanie"),
 
     url(r'^test/$', views.test_lista, name='test_lista'),
     url(r'^test/dodaj/$', views.test_dodaj, name='test_dodaj'),
     url(r'^test/(?P<id>[1-9]+)/$', views.test_szczegoly, name='test_szczegoly'),
-	url(r'^test/(?P<id>[1-9]+)/usun/$', views.test_usun, name='test_usun'),
-	url(r'^test/(?P<id>[1-9]+)/edytuj/$', views.test_edytuj, name='test_edytuj'),
+    url(r'^test/(?P<id>[1-9]+)/usun/$', views.test_usun, name='test_usun'),
+    url(r'^test/(?P<id>[1-9]+)/edytuj/$', views.test_edytuj, name='test_edytuj'),
 
-	url(r'^sprawdzian/$', views.sprawdzian_lista, name='sprawdzian_lista'),
+    url(r'^sprawdzian/$', views.sprawdzian_lista, name='sprawdzian_lista'),
     url(r'^sprawdzian/dodaj/$', views.sprawdzian_dodaj, name='sprawdzian_dodaj'),
     url(r'^sprawdzian/(?P<id>[1-9]+)/$', views.sprawdzian_szczegoly, name='sprawdzian_szczegoly'),
     url(r'^sprawdzian/(?P<id>[1-9]+)/usun/$', views.sprawdzian_usun, name='sprawdzian_usun'),
