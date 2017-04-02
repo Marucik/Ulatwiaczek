1db96756f9c537273679a5121191121586f34e69
Marucik <rakoczy.mk@gmail.com>
2017-04-02 11:52:22 +0200

    Zmiana nazwy projektu z "app" > "ulatwiaczek"

--- ulatwiaczek/settings.py
+++ ulatwiaczek/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'ulatwiaczek.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'ulatwiaczek.wsgi.application'
 
 
 # Database
