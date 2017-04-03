a84546a8af04d435d0060c5c18f6761fb00c4478
Dawiducik <edekdw@gmail.com>
2017-04-03 22:32:07 +0200

    Zaimplementowano aplikacje 'home' w głównym pliku urls.py projektu

--- ulatwiaczek/urls.py
+++ ulatwiaczek/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('main.urls')),
+    url(r'^ulatwiaczek/', include('main.urls')),
+    url(r'^', include('home.urls')),
 ]
