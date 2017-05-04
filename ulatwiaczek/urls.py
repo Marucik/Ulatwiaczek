8f12d6566400a52314b22f057bea387ec2ac0008
Dawiducik <edekdw@gmail.com>
2017-05-04 23:06:01 +0200

    Dodano url do panelu admina

--- ulatwiaczek/urls.py
+++ ulatwiaczek/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('home.urls')),
-    url(r'^ulatwiaczek/', include('main.urls')),
-    url(r'^users/', include('users.urls')),
+    url(r'^', include('home.urls', namespace="home")),
+    url(r'^ulatwiaczek/', include('main.urls', namespace="main")),
+    url(r'^konto/', include('users.urls', namespace="users")),
+    url(r'^admin/', admin.site.urls),
 ]
