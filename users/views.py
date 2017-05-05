f29c91ea499a23492c4bf6507f1d65dfaf9a0e7c
Marucik <rakoczy.mk@gmail.com>
2017-05-05 19:56:55 +0200

    Jeżeli użytkownik jest zalogowany, zostaje przeniesiony na stronę główną

--- users/views.py
+++ users/views.py
@@ -7,25 +7,24 @@ from django.core.exceptions import FieldError
 
 # Create your views here.
 def login_user(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('main:test_lista')
-        else:
-            return HttpResponse("Nie znaleziono lub nieprawidłowe dane")
+    if request.user.is_authenticated:
+        return redirect('main:test_lista')
     else:
-        return render(request, 'users/login.html')
-
-
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main:test_lista')
+            else:
+                return HttpResponse("Nie znaleziono lub nieprawidłowe dane")
+        else:
+            return render(request, 'users/login.html')
 
 def register_user(request):
     return render(request, 'users/register.html')
 
 def logout_user(request):
     logout(request)
-    return redirect('users:login')
-
-
+    return render(request, 'users/logout.html')
