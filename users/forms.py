feb74587c25d5c2d522fcd90dc807b3545a4fa77
Dawiducik <edekdw@gmail.com>
2017-05-08 17:52:40 +0200

    dodano rejestrację użytkownika

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        label="Imię",
+        required=False,
+        max_length=100,
+        help_text="Opcjonalnie."
+    )
+    class Meta:
+        model = User
+        fields = ( "username", "first_name", "password1", "password2", )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseNotFound, Http404, JsonResponse, HttpResponseRedirect
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import FieldError
+from django.http import (Http404, HttpResponse, HttpResponseNotFound,
+                         HttpResponseRedirect, JsonResponse)
+from django.shortcuts import get_object_or_404, redirect, render
+
+from users.forms import RegisterForm
+
 
 # Create your views here.
 def login_user(request):
@@ -23,7 +28,25 @@ def login_user(request):
             return render(request, 'users/login.html')
 
 def register_user(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('main:index')
+        else: 
+            return render(request, 'users/register.html', {
+                'register_form': form,
+                'register_error': True,
+            })
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {
+        'register_form': form,
+    })
 
 def logout_user(request):
     logout(request)
