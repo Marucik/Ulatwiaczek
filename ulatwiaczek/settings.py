b60eea7ac442d17043d0af162f704f394441a52d
Marucik <rakoczy.mk@gmail.com>
2017-06-11 13:52:54 +0200

    Mała zmiana w ścieżce do static'ów

--- ulatwiaczek/settings.py
+++ ulatwiaczek/settings.py
@@ -150,14 +150,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
+STATIC_URL = '/ulatwiaczek/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
+
 STATICFILES_DIRS = (
     'ulatwiaczek/static/',
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
 LOGIN_URL = '/konto/zaloguj/'
