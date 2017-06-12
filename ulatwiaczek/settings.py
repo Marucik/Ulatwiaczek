2fe2d06ed3afef914877760900ff0bf2553a5ce5
solus <xinulsw@gmail.com>
2017-06-12 22:30:36 +0200

    Kolejna próba uruchomienia staticfiles

--- ulatwiaczek/settings.py
+++ ulatwiaczek/settings.py
@@ -149,13 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
+    os.path.join(BASE_DIR, '/ulatwiaczek/static/'),
 ]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/konto/zaloguj/'
