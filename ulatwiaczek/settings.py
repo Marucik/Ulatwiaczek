29f47c1711ca4619ea04cc8a614d51e26dc2256f
solus <xinulsw@gmail.com>
2017-06-13 13:51:45 +0200

    Zmiana lokalizacji static.

--- ulatwiaczek/settings.py
+++ ulatwiaczek/settings.py
@@ -152,9 +152,6 @@ USE_TZ = True
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/ulatwiaczek/static/'),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/konto/zaloguj/'
